bcca71f8867d1220ed76d477ae2b015b20e41f2b
agilbiaviv <agil.biaviv@gmail.com>
2021-09-13 07:49:24 +0700

    update skor.js

--- assets/js/skor.js
+++ assets/js/skor.js
@@ -16,32 +16,33 @@ const getUrlParameter = (sParam) => {
   return false;
 };
 
-const hitungSkor = async() => {
+const hitungSkor = async () => {
   let skor = {
-    twk : 0,
-    tiu : 0,
-    tkp : 0
+    twk: 0,
+    tiu: 0,
+    tkp: 0
   }
   let paket = getUrlParameter('paket')
+  $('#judul').text(`Hasil Tryout Paket ${paket}`);
   let res = await fetch(`./assets/paketFree/${paket}.json`)
   let dataSoal = await res.json()
   let arrJawaban = JSON.parse(localStorage.getItem('jawaban'))
 
-  for(i = 0; i < arrJawaban.length; i++) {
-    if(i < 30) { // twk sampe nomor 30
-      if(arrJawaban[i] == dataSoal[i].kunci) { // jika jawaban == kunci
+  for (i = 0; i < arrJawaban.length; i++) {
+    if (i < 30) { // twk sampe nomor 30
+      if (arrJawaban[i] == dataSoal[i].kunci) { // jika jawaban == kunci
         skor.twk += 5
       }
     }
 
-    if(i >= 30 && i < 65 ) { // tiu 31 - 65
-      if(arrJawaban[i] == dataSoal[i].kunci) {
+    if (i >= 30 && i < 65) { // tiu 31 - 65
+      if (arrJawaban[i] == dataSoal[i].kunci) {
         skor.tiu += 5
       }
     }
 
-    if(i >= 65) {
-      if(arrJawaban[i] == null || arrJawaban[i] == "") {
+    if (i >= 65) {
+      if (arrJawaban[i] == null || arrJawaban[i] == "") {
         continue
       }
 
@@ -56,25 +57,25 @@ const hitungSkor = async() => {
 }
 
 const showSkor = (skor) => {
-  let {twk, tiu, tkp} = skor
+  let { twk, tiu, tkp } = skor
   let passGrade = {
-    twk : 65,
-    tiu : 80,
-    tkp : 166
+    twk: 65,
+    tiu: 80,
+    tkp: 166
   }
   let ket = {
-    lolos : `<span class="badge bg-primary">Lolos nilai <i>passing grade</i></span>`,
-    gagal : `<span class="badge bg-danger">Tidak lolos <i>passing grade</i></span>`
+    lolos: `<span class="badge bg-primary">Lolos nilai <i>passing grade</i></span>`,
+    gagal: `<span class="badge bg-danger">Tidak lolos <i>passing grade</i></span>`
   }
   $('#skor-twk').text(twk)
   $('#skor-tiu').text(tiu)
   $('#skor-tkp').text(tkp)
-  $('#total-skor').text( twk + tiu + tkp )
-  $('#ket-twk').html(twk > passGrade.twk ?  ket.lolos : ket.gagal)
-  $('#ket-tiu').html(tiu > passGrade.tiu ?  ket.lolos : ket.gagal)
-  $('#ket-tkp').html(tkp > passGrade.tkp ?  ket.lolos : ket.gagal)
+  $('#total-skor').text(twk + tiu + tkp)
+  $('#ket-twk').html(twk > passGrade.twk ? ket.lolos : ket.gagal)
+  $('#ket-tiu').html(tiu > passGrade.tiu ? ket.lolos : ket.gagal)
+  $('#ket-tkp').html(tkp > passGrade.tkp ? ket.lolos : ket.gagal)
 
-  if(tiu > passGrade.tiu && twk > passGrade.twk && tkp > passGrade.tkp) {
+  if (tiu > passGrade.tiu && twk > passGrade.twk && tkp > passGrade.tkp) {
     $('#keterangan-lolos')
       .addClass('text-primary')
       .html(`Selamat, anda <strong>lolos</strong> ke tahap selanjutnya!`)
